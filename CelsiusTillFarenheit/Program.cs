3584c79f87859fa44424b0a4e7e67b758d8a413b
Stefan Sjöberg <bird.nerd@outlook.com>
2019-03-31 12:42:46 +0200

    Add project files.

---
+++ CelsiusTillFarenheit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsiusTillFarenheit
+{
+    class Program
+    {
+        static double ToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius / 5.0) * 9 + 32;
+            return fahrenheit;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Konvertera Celsius till Fahrenheit: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Celsius till Fahrenheit är: " + ToFahrenheit(number));
+
+            Console.Read();
+        }
+    }
+}
